0aa063e6a62df5110f3b3869e908fc0e95031a58
ViavaCos <13811092694@163.com>
2021-05-25 21:19:26 +0800

    add component button

---
+++ src/components/button/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="v-button" @click="$emit('click')">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'v-button',
+    props: {
+        
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import './index.less';
+</style>
